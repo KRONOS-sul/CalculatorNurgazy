c3780b1c6e9b0d79b06da46e06493860679bc356
Sultan <93419011+KRONOS-sul@users.noreply.github.com>
2024-05-07 15:32:42 +0600

    Homework 8

--- app/src/main/java/com/example/calculatornurgazy/BoredActivity.kt
+++ app/src/main/java/com/example/calculatornurgazy/BoredActivity.kt
@@ -3,12 +3,30 @@ package com.example.calculatornurgazy
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.example.calculatornurgazy.databinding.ActivityBoredBinding
 
 class BoredActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityBoredBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_bored)
+        binding = ActivityBoredBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        with(binding) {
+            nextBtn.setOnClickListener { finishAffinity() }
+
+            favouriteBtn.tag = R.drawable.ic_heart
+            favouriteBtn.setOnClickListener {
+                val currentImage = favouriteBtn.tag
+                if (currentImage == R.drawable.ic_heart) {
+                    favouriteBtn.setImageResource(R.drawable.ic_full_heart)
+                    favouriteBtn.tag = R.drawable.ic_full_heart
+                } else {
+                    favouriteBtn.setImageResource(R.drawable.ic_heart)
+                    favouriteBtn.tag = R.drawable.ic_heart
+                }
+            }
+        }
     }
 }
